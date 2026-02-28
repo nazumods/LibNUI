42b3d65fe0f8ff0bd016d511c0fd3088a9f97776
nazu <nazumods@gmail.com>
2026-02-28 08:58:46 -0500

    test: update for multiple tests

--- globals.lua
+++ globals.lua
@@ -11,5 +11,5 @@ end)
 
 ns:registerCommand("test", nil, function(self, msg)
   C_AddOns.LoadAddOn("LibNUI_Test")
-  if LibNUITest then LibNUITest.run() end
+  if LibNUITest then LibNUITest.run(msg) end
 end, "Show TableFrame test windows")
